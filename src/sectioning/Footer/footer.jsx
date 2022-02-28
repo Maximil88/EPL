bf958817390ef67ddcf51ede9a518a467fb5bd75
Maximil88 <87375553+Maximil88@users.noreply.github.com>
2022-02-28 13:28:06 -0500

    altered footer

--- src/sectioning/Footer/footer.jsx
+++ src/sectioning/Footer/footer.jsx
@@ -3,7 +3,7 @@ import './style.css';
 export default function footer() {
   return (
     <div className="footer">
-        <a class = "email" href= "mailto: maxbb329@outlook.com">maxbb329@outlook.com</a>
+        <a class="email" href= "mailto: maxbb329@outlook.com">maxbb329@outlook.com</a>
     </div>
   )
 }
